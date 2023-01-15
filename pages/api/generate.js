e76065f7cc75dd9801aba103a5fede01d18c3920
fleeB1 <fowler.lee8@gmail.com>
2023-01-16 00:16:25 +0100

    buildspace learn with farza

---
+++ pages/api/generate.js
@@ -0,0 +1,51 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = 
+`
+Write me a detailed table of contents for a blog post with the title below.
+
+Title:
+`
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  const secondPrompt = 
+  `
+  Take the table of contents and title of the blog post below and generate a blog post written in the style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+
+  Title: ${req.body.userInput}
+
+  Table of Contents: ${basePromptOutput.text}
+
+  Blog Post:
+  `
+
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    temperature: 0.75,
+    max_tokens: 380,
+  });
+  
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
